7cb3c65a4e9233fe7d13267d6c4c49477292952a
YousufMohammadi <yousufmohammadi3222@gmail.com>
2022-12-20 22:05:25 +0430

    sideBar section created

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,9 +1,11 @@
 import { Header } from "../StaticComponents/Header";
+import { SideBar } from "../StaticComponents/SideBar";
 
 export function Home() {
     return(
         <div className="home-container">
             <Header />
+            <SideBar />
         </div>
     )
 }
---
+++ src/Components/StaticComponents/SideBar.jsx
@@ -0,0 +1,8 @@
+import "../../Styles/StaticComponents/SideBar.css"
+export function SideBar() {
+    return(
+        <div className="sideBar-container">
+            this is sideBar
+        </div>
+    )
+}
