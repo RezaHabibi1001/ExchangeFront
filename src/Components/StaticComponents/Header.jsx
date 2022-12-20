df974901095e4730e240736235a146a913aaa79c
YousufMohammadi <yousufmohammadi3222@gmail.com>
2022-12-20 21:17:03 +0430

    header section created

--- src/Components/StaticComponents/Header.jsx
+++ src/Components/StaticComponents/Header.jsx
@@ -1,7 +1,11 @@
+import "../../Styles/StaticComponents/Global.css"
+import "../../Styles/StaticComponents/Header.css"
 export function Header() {
     return(
         <div className="header-container">
-            this is header
+            <a href="#"><img src="turn-off.png" alt="خروج" /></a>
+            <span className="header-heading">سیستم مدیریتی اکسیر</span>
+            <a href="#"><img src="pharmacy.png" alt="لوگو" /></a>
         </div>
     )
 }
