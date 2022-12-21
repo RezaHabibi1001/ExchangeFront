96aa69e576462b67aac8d75266737c043ddbac6e
YousufMohammadi <yousufmohammadi3222@gmail.com>
2022-12-21 08:27:01 +0430

    dropdown components now can taked props value

--- src/Components/StaticComponents/DropDownButton.jsx
+++ src/Components/StaticComponents/DropDownButton.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react"
 import "../../Styles/StaticComponents/DropDownButton.css"
-export function DropDownButton(params) {
+export function DropDownButton(props) {
     const [display,setDisplay] = useState('none')
     function showDropDown() {
         if (display == 'none') {
@@ -10,14 +10,17 @@ export function DropDownButton(params) {
           setDisplay('none')
         }
       }
+      const dropdownName = props.dropdownName
+      const dropdownLink = props.dropdownLink
     return(
         <div className="dropdown">
-            <button className="drop-btn" onClick={showDropDown}>گدام ها</button>
+            <button className="drop-btn" onClick={showDropDown}>{dropdownName}</button>
                 <div id="dropdownContent" style={{display:display}} className="dropdown-content">
-                    <a href="#">ثبت گدام</a>
-                    <a href="#"> لیست دارو ها</a>
-                    <a href="#">ثبت نوعیت</a>
-                    <a href="#"> لیست نوعیت ها</a>
+                   {
+                    dropdownLink.map(link=>{
+                        return <a href={link.linkUrl}>{link.linkName}</a>
+                    })
+                   }                   
                 </div>
         </div>
     )
--- src/Components/StaticComponents/SideBar.jsx
+++ src/Components/StaticComponents/SideBar.jsx
@@ -1,12 +1,11 @@
-import { useState } from "react"
 import "../../Styles/StaticComponents/SideBar.css"
 import { DropDownButton } from "./DropDownButton"
 export function SideBar() {
     return(
         <div className="sideBar-container">
-            <DropDownButton />
-            <DropDownButton />
-            <DropDownButton />
+            <DropDownButton  
+            dropdownName="دارو ها" 
+            dropdownLink={[{linkName:'ثبت دارو',linkUrl:'Home'},{linkName:'لیست دارو',linkUrl:'FARHOME'}]} />
         </div>
     )
 }
