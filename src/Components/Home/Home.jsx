423ab1004062e7cd94668775041f3247e7b85b8b
YousufMohammadi <yousufmohammadi3222@gmail.com>
2022-12-21 23:52:49 +0430

    content section created

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,11 +1,14 @@
 import { Header } from "../StaticComponents/Header";
 import { SideBar } from "../StaticComponents/SideBar";
-
+import "../../Styles/StaticComponents/Global.css"
 export function Home() {
     return(
         <div className="home-container">
             <Header />
             <SideBar />
+            <div className="content">
+                this is content
+            </div>
         </div>
     )
 }
