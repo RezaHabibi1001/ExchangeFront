cebfbeeb0bcc293994fac2d036d9124f84913402
YousufMohammadi <yousufmohammadi3222@gmail.com>
2022-12-21 22:44:18 +0430

    colors palets added

--- src/Components/StaticComponents/SideBar.jsx
+++ src/Components/StaticComponents/SideBar.jsx
@@ -5,7 +5,12 @@ export function SideBar() {
         <div className="sideBar-container">
             <DropDownButton  
             dropdownName="دارو ها" 
-            dropdownLink={[{linkName:'ثبت دارو',linkUrl:'Home'},{linkName:'لیست دارو',linkUrl:'FARHOME'}]} />
+            dropdownLink={[{linkName:'ثبت دارو',linkUrl:'Home'},{linkName:'لیست دارو',linkUrl:'FARHOME'}]} 
+            />
+            <DropDownButton  
+            dropdownName="دارو ها" 
+            dropdownLink={[{linkName:'ثبت دارو',linkUrl:'Home'},{linkName:'لیست دارو',linkUrl:'FARHOME'}]} 
+            />
         </div>
     )
 }
