97900411c7bd1ae4a310a3177f7f424e287a1de0
YousufMohammadi <yousufmohammadi3222@gmail.com>
2022-12-30 17:25:34 +0430

    factor header created

--- src/Components/Factors/BuyFactor.jsx
+++ src/Components/Factors/BuyFactor.jsx
@@ -1,6 +1,6 @@
 import { Header } from "../StaticComponents/Header";
 import { SideBar } from "../StaticComponents/SideBar";
-
+import "../../Styles/Factors/Factors.css"
 export function BuyFactor() {
     return(
         <div className="container">
@@ -8,7 +8,40 @@ export function BuyFactor() {
             <SideBar />
             <div className="content">
                 <div className="buyFactor-container">
-                    this is buy factor
+                    <div className="factor-header">
+                        <div className="header-row">
+                            <div className="group">
+                                <span>طرف حساب</span>
+                                <input type="text" />
+                            </div>
+                            {/* <div className="group">
+                                <span>کد</span>
+                                <span>00023</span>
+                            </div> */}
+                            <div className="group">
+                                <span>تلفن</span>
+                                <span>0098790883362</span>
+                            </div>
+                            <div className="group">
+                                <span>شماره فاکتور</span>
+                                <span>12340</span>
+                            </div>
+                        </div>
+                        <div className="header-row">
+                            <div className="group">
+                                <span>آدرس</span>
+                                <span>مارکت ابن سینا</span>
+                            </div>
+                            <div className="group">
+                                <span>مانده</span>
+                                <span>91388  بد</span>
+                            </div>
+                            <div className="group">
+                                <span>تاریخ</span>
+                                <span>1401 / 02 / 15</span>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
