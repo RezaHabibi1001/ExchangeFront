54b952ab58b99ed372c173eaba7a84ed9d05f1df
YousufMohammadi <yousufmohammadi3222@gmail.com>
2023-01-03 21:27:28 +0430

    users restyled

--- src/Components/Users/User.jsx
+++ src/Components/Users/User.jsx
@@ -17,7 +17,176 @@ export function Users() {
                     <span className="btn ">لیست کاربران</span>
                 </div>
                 <div className="users-content">
-                   <Table />
+                <div className="table-container">
+            <table>
+                <thead>
+                    <tr>
+                        <th className="user-code">ردیف</th>
+                        <th className="user-name">نام</th>
+                        <th className="user-userName">نام کاربری</th>
+                        <th className="user-password">پسورد</th>
+                        <th className="user-type">صلاحیت</th>
+                        <th className="user-action">عملیات</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>1</td>
+                        <td>احمد</td>
+                        <td>yosufmohammadi</td>
+                        <td>yousuf123</td>
+                        <td>ادمین</td>
+                        <td className="action">
+                            <img src="edit.png" alt="edit" />
+                            <img src="delete.png" alt="delete" />
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>1</td>
+                        <td>احمد</td>
+                        <td>yosufmohammadi</td>
+                        <td>yousuf123</td>
+                        <td>ادمین</td>
+                        <td className="action">
+                            <img src="edit.png" alt="edit" />
+                            <img src="delete.png" alt="delete" />
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>1</td>
+                        <td>احمد</td>
+                        <td>yosufmohammadi</td>
+                        <td>yousuf123</td>
+                        <td>ادمین</td>
+                        <td className="action">
+                            <img src="edit.png" alt="edit" />
+                            <img src="delete.png" alt="delete" />
+                        </td>
+                    </tr>         
+                    <tr>
+                        <td>1</td>
+                        <td>احمد</td>
+                        <td>yosufmohammadi</td>
+                        <td>yousuf123</td>
+                        <td>ادمین</td>
+                        <td className="action">
+                            <img src="edit.png" alt="edit" />
+                            <img src="delete.png" alt="delete" />
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>1</td>
+                        <td>احمد</td>
+                        <td>yosufmohammadi</td>
+                        <td>yousuf123</td>
+                        <td>ادمین</td>
+                        <td className="action">
+                            <img src="edit.png" alt="edit" />
+                            <img src="delete.png" alt="delete" />
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>1</td>
+                        <td>احمد</td>
+                        <td>yosufmohammadi</td>
+                        <td>yousuf123</td>
+                        <td>ادمین</td>
+                        <td className="action">
+                            <img src="edit.png" alt="edit" />
+                            <img src="delete.png" alt="delete" />
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>1</td>
+                        <td>احمد</td>
+                        <td>yosufmohammadi</td>
+                        <td>yousuf123</td>
+                        <td>ادمین</td>
+                        <td className="action">
+                            <img src="edit.png" alt="edit" />
+                            <img src="delete.png" alt="delete" />
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>1</td>
+                        <td>احمد</td>
+                        <td>yosufmohammadi</td>
+                        <td>yousuf123</td>
+                        <td>ادمین</td>
+                        <td className="action">
+                            <img src="edit.png" alt="edit" />
+                            <img src="delete.png" alt="delete" />
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>1</td>
+                        <td>احمد</td>
+                        <td>yosufmohammadi</td>
+                        <td>yousuf123</td>
+                        <td>ادمین</td>
+                        <td className="action">
+                            <img src="edit.png" alt="edit" />
+                            <img src="delete.png" alt="delete" />
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>1</td>
+                        <td>احمد</td>
+                        <td>yosufmohammadi</td>
+                        <td>yousuf123</td>
+                        <td>ادمین</td>
+                        <td className="action">
+                            <img src="edit.png" alt="edit" />
+                            <img src="delete.png" alt="delete" />
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>1</td>
+                        <td>احمد</td>
+                        <td>yosufmohammadi</td>
+                        <td>yousuf123</td>
+                        <td>ادمین</td>
+                        <td className="action">
+                            <img src="edit.png" alt="edit" />
+                            <img src="delete.png" alt="delete" />
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>1</td>
+                        <td>احمد</td>
+                        <td>yosufmohammadi</td>
+                        <td>yousuf123</td>
+                        <td>ادمین</td>
+                        <td className="action">
+                            <img src="edit.png" alt="edit" />
+                            <img src="delete.png" alt="delete" />
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>1</td>
+                        <td>احمد</td>
+                        <td>yosufmohammadi</td>
+                        <td>yousuf123</td>
+                        <td>ادمین</td>
+                        <td className="action">
+                            <img src="edit.png" alt="edit" />
+                            <img src="delete.png" alt="delete" />
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>1</td>
+                        <td>احمد</td>
+                        <td>yosufmohammadi</td>
+                        <td>yousuf123</td>
+                        <td>ادمین</td>
+                        <td className="action">
+                            <img src="edit.png" alt="edit" />
+                            <img src="delete.png" alt="delete" />
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
                 </div>
             </div>
             </div>
