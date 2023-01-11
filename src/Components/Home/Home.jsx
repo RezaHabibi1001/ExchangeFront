099c824e89e9e838cefae106af39547f14a2087e
mansourQaderi <mansourqaderi21@gmail.com>
2023-01-11 20:10:57 +0430

    the login design finalized

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,29 +1,21 @@
 import { Header } from "../StaticComponents/Header";
 import { SideBar } from "../StaticComponents/SideBar";
-import "../../Styles/StaticComponents/Global.css"
-import "../../Styles/Home/Home.css"
+import "../../Styles/StaticComponents/Global.css";
+import "../../Styles/Home/Home.css";
 import { Card } from "../StaticComponents/Card";
 export function Home() {
-    return(
-        <div className="container">
-            <Header />
-            <SideBar />
-            <div className="content"> 
-                <div className="card-container">
-                    <div className="row">
-                        <div className="card-parent"><Card cardTitle="دارو ها" cardCount="2000" cardImg="drugss.png"/></div>
-                        <div className="card-parent"><Card cardTitle="فاکتور ها" cardCount="1000" cardImg="factors.png"/></div>
-                        <div className="card-parent"><Card cardTitle="مشتریان" cardCount="2000" cardImg="customers.png"/></div>
-                    </div>
-
-                    <div className="row">
-                        <div className="card-parent"><Card cardTitle="اسناد" cardCount="3000" cardImg="bills.png" /></div>
-                        <div className="card-parent"><Card cardTitle="گدام ها" cardCount="1000" cardImg="stacks.png" /></div>
-                        <div className="card-parent"><Card cardTitle="کاربران" cardCount="5" cardImg="users.png" /></div>
-                    </div>
-                  
-                </div>
-            </div>
+  return (
+    <div className="container">
+      <div className="login-container">
+        <div className="login-header-container">
+          <img src="factors.png" alt="loginImage" className="login-image" />
+          <span className="login-header">ورود به برنامه حسابداری</span>
+          <img src="factors.png" alt="loginImage" className="login-image" />
         </div>
-    )
+        <input type="text" placeholder="شماره تماس" className="login-input" />
+        <input type="text" placeholder="رمز عبور" className="login-input" />
+        <input type="button" value="ورود به برنامه" className="login-button" />
+      </div>
+    </div>
+  );
 }
