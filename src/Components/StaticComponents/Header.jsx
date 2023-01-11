be008246d13c4b83f6abe0b0962a1cd3af87d1c2
mansourQaderi <mansourqaderi21@gmail.com>
2023-01-11 20:45:59 +0430

    header finalized

--- src/Components/StaticComponents/Header.jsx
+++ src/Components/StaticComponents/Header.jsx
@@ -1,12 +1,16 @@
-import { Link } from "react-router-dom"
-import "../../Styles/StaticComponents/Global.css"
-import "../../Styles/StaticComponents/Header.css"
+import { Link } from "react-router-dom";
+import "../../Styles/StaticComponents/Global.css";
+import "../../Styles/StaticComponents/Header.css";
 export function Header() {
-    return(
-        <div className="header-container">
-            <a href="#"><img src="turn-off.png" alt="خروج" /></a>
-            <span className="header-heading">سیستم مدیریتی داروی اکسیر</span>
-            <Link to={"/home"}><img src="pharmacy.png" alt="لوگو" /></Link>
-        </div>
-    )
+  return (
+    <div className="header-container">
+      <Link to={"/"}>
+        <img src="turn-off.png" alt="خروج" />
+      </Link>
+      <span className="header-heading">سیستم حسابداری صرافی</span>
+      <Link to={"#"}>
+        <img src="pharmacy.png" alt="لوگو" />
+      </Link>
+    </div>
+  );
 }
