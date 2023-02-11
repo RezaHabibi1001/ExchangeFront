7673861ea5d7f252f73a1af335c6001aec4e4fdb
mansourQaderi <mansourqaderi21@gmail.com>
2023-02-11 14:57:05 +0430

    change the amoiut of market

--- src/Components/Market/Market.jsx
+++ src/Components/Market/Market.jsx
@@ -1,96 +1,65 @@
 import { Header } from "../StaticComponents/Header";
-import { Search } from "../StaticComponents/Search";
 import { SideBar } from "../StaticComponents/SideBar";
 import { useState } from "react";
 import "../../Styles/Market/Market.css";
 
 export function Market() {
-  const [addPopUp, setAddPopUp] = useState(false);
-  const [data, setData] = useState([]);
+  const [marketValue, setMarketValue] = useState(3843);
+  const [editPopUp, setEditPopUp] = useState(false);
+
+  const editMarket = (e) => {
+    e.preventDefault();
+    setMarketValue(e.target.marketValue.value);
+    setEditPopUp(false);
+  };
   return (
     <div className="container">
       <Header />
       <SideBar />
-      {addPopUp ? (
-        <div className="addDrug-content">
-          <div className="addDrug-header">
-            <img
-              src="close.png"
-              alt="close"
-              onClick={() => setAddPopUp(false)}
-            />
-            <span>افزودن حواله جدید</span>
-            <img src="bills.png" alt="logo" />
-          </div>
-          <div className="addDrug-body">
-            <input
-              type="text"
-              name="sendNumber"
-              placeholder="نمبر حواله"
-              className="addDrug-body-input"
-            />
-            <input
-              type="text"
-              name="sendNumber"
-              placeholder="نام مشتری"
-              className="addDrug-body-input"
-            />
-            <input
-              type="text"
-              name="sendNumber"
-              placeholder="کشور/ شهر "
-              className="addDrug-body-input"
-            />{" "}
-            <input
-              type="text"
-              name="sendNumber"
-              placeholder="واحد پول"
-              className="addDrug-body-input"
-            />{" "}
-            <input
-              type="text"
-              name="sendNumber"
-              placeholder="مبلغ حواله"
-              className="addDrug-body-input"
-            />{" "}
-            <input
-              type="text"
-              name="sendNumber"
-              placeholder="قیمت خرید"
-              className="addDrug-body-input"
-            />{" "}
-            <input
-              type="text"
-              name="sendNumber"
-              placeholder="قیمت فروش"
-              className="addDrug-body-input"
-            />{" "}
-            <input
-              type="text"
-              name="sendNumber"
-              placeholder="مقدار مفاد"
-              className="addDrug-body-input"
-            />
-            <input
-              type="button"
-              value="ثبت حواله"
-              className="addDrug-body-button"
-            />
-          </div>
-        </div>
-      ) : null}
+
       <div className="content">
         <div className="market-container">
+          {editPopUp ? (
+            <div className="editMarket-content">
+              <div className="addDrug-header">
+                <img
+                  src="close.png"
+                  alt="close"
+                  onClick={() => setEditPopUp(false)}
+                />
+                <span>ویرایش موجودی مارکت</span>
+                <img src="bills.png" alt="logo" />
+              </div>
+              <form onSubmit={editMarket}>
+                <div className="addDrug-body">
+                  <input
+                    defaultValue={marketValue}
+                    type="text"
+                    name="marketValue"
+                    placeholder="مقدار موجودی"
+                    className="addDrug-body-input"
+                  />
+
+                  <input
+                    type="submit"
+                    value="ثبت تغییرات"
+                    className="addDrug-body-button"
+                  />
+                </div>
+              </form>
+            </div>
+          ) : null}
           <div className="repository-container">
             <div className="repository-header">
               <img
+                onClick={() => setEditPopUp(true)}
                 src="edit.png"
                 className="repository-header-img"
                 alt="edit"
               />
             </div>
             <span className="repository-title">موجودی کل</span>
-            <span className="repository-amount">798789</span>
+            <span className="repository-amount">{marketValue}</span>
           </div>
         </div>
       </div>
